3e8d379d577f552c2a64db6f5c49c91e7de90226
james tak <jtak93@gmail.com>
2019-06-09 18:02:41 -0700

    creates grid component

---
+++ client/src/components/Grid/index.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'; // let's also import Component
+
+export class Grid extends Component<{}> {
+
+    render() {
+        return <div>My Grid</div>;
+    }
+}
+
+export default Grid;
