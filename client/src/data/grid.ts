6be33ea81f5dfd901f49debba6f2edc5976e0b08
james tak <jtak93@gmail.com>
2019-06-09 20:53:55 -0700

    adds basic grid generation function

--- client/src/data/grid.ts
+++ client/src/data/grid.ts
@@ -1,3 +1,7 @@
+interface Tile {
+    bgColor: string;
+}
+
 const emptyTile = {
     bgColor: "#228B22"
 }
@@ -6,14 +10,43 @@ const wallTile = {
     bgColor: "#D3D3D3"
 }
 
+const tileArray = [emptyTile, wallTile];
+
+/**
+ * @return 2D Array of grid tiles
+ * @param row 
+ * @param col 
+ */
+const generateNewGrid = (rowLen = 14, colLen = 14) => {
+    console.log('generating new grid')
+    const ret:Tile[][] = [];
+    // generate top wall
+    const wallRow: Tile[] = []
+    for (let i = 0; i < rowLen; i++) {
+        wallRow.push(wallTile);
+    }
+    ret.push(wallRow)
+    // generate rows
+    for (let i = 1; i < colLen - 1; i++) {
+        const row: Tile[] = [];
+        row.push(wallTile);
+        for (let j = 1; j < rowLen - 1; j++) {
+            row.push(getRandomTile(tileArray))
+        }
+        row.push(wallTile)
+        ret.push(row)
+    } 
+    ret.push(wallRow)
+    return ret;
+}
+
+const getRandomTile = (tiles: Tile[]) => {
+    return tiles[Math.floor(Math.random() * tiles.length)];
+}
 
 
 const gridData = {
-    grid: [
-        [wallTile, wallTile, wallTile, wallTile, wallTile, wallTile, wallTile, wallTile],
-        [wallTile, emptyTile, emptyTile, emptyTile, wallTile, emptyTile, emptyTile, wallTile],
-        [wallTile, wallTile, wallTile, wallTile, wallTile, wallTile, wallTile, wallTile],
-    ]
+    grid: generateNewGrid()
 }
 
 export default gridData;
